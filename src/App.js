be20f22128df383fc647306b2c4716fa6c026270
SCassellius <46692839+SCassellius@users.noreply.github.com>
2020-01-29 11:51:43 -0800

    added adding functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState} from 'react';
 import TodoList from "./todo"
+import TodoEntryForm from './TodoEntryForm';
 import './App.css';
 
 export default function App() {
@@ -10,10 +11,14 @@ export default function App() {
   function deleteTodo(id) {
        setTodos(todos => todos.filter(todo => todo.id !== id));
  }
+ function addTodo(todo) {
+  setTodos(todos => [...todos, todo]);
+}
   return (
     <div className="App">
       <h1>Things To Do</h1>
       <TodoList todos={todos} deleter={deleteTodo} />
+      <TodoEntryForm adder={addTodo} />
     </div>
   );
 }
